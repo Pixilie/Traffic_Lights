1d48022fcfab1fb32627718d1cb8757ffc9cf607
Nhitros <77635843+Nhitros@users.noreply.github.com>
2023-02-27 19:37:48 +0100

    MAIN.PY buttons change position
    We change the place of the button to fit with the screen

--- mainMenu.py
+++ mainMenu.py
@@ -42,44 +42,39 @@ def Credits():
     mainWindow.destroy()
 
 
-# Main frame
-frame = Frame(mainWindow, bg='#B78BC4')
-
 
 # Title
 title_icon = PhotoImage(file="./Menus/Assets/traffic_lights.png")
-title = Canvas(mainWindow, width=screenWidth*0.7, height=screenHeight*0.5,
+title = Canvas(mainWindow, width=screenWidth*0.7, height=screenHeight*0.35,
                bg='#B78BC4', bd=0, highlightthickness=0)
 title.create_image(screenWidth*0.35, screenHeight*0.2, image=title_icon)
-title.pack(pady=20)
+title.pack(side='top')
 
 # Exit button
-exitIcon = PhotoImage(
-    file='./Menus/Assets/exit_icon.png', width=round(screenWidth*0.08), height=round(screenHeight*0.09))  # TODO: enlever bordure quand survoler + solution pour responsive taille car n'accepte pas les float
+exitIcon = PhotoImage(file='./Menus/Assets/exit_icon.png', width=round(screenWidth*0.08), height=round(screenHeight*0.09))  # TODO: enlever bordure quand survoler + solution pour responsive taille car n'accepte pas les float
 exitButton = Button(mainWindow, image=exitIcon,
                     bd=0, bg='#B78BC4', highlightthickness=0, command=Close)
-exitButton.pack(pady=800)
-exitButton.place(x=screenWidth*0.93, y=screenHeight*0.90)
+exitButton.pack()
+exitButton.place(x=screenWidth - 100, y=screenHeight - 71)
 
 # TODO: animation de survole des boutons + son + peut-être les mettre dans un canvas et les centrer dans le canvas
 # Levels Button
 levelsButton = Button(mainWindow, text="Niveaux", font=('Arial', round(screenWidth*0.016)),
                       bd=0, relief="flat", activebackground="#c59dd1", activeforeground="white", bg='#B78BC4', fg="#ffffff", width=round(screenWidth*0.006), command=Levels)
-levelsButton.pack(pady=20)
-levelsButton.place(x=screenWidth*0.44, y=screenHeight*0.35)
+levelsButton.pack(side = 'top')
+#levelsButton.place(x=screenWidth*0.44, y=screenHeight*0.35)
 
 # Setting Button
 settingsButton = Button(mainWindow, text="Paramètres", font=('Arial', round(screenWidth*0.016)),
                         bd=0, relief="flat", activebackground="#c59dd1", activeforeground="white", bg='#B78BC4', fg="#ffffff", width=round(screenWidth*0.008), state="disabled", command=Settings)
-settingsButton.pack(pady=20)
-settingsButton.place(x=screenWidth*0.43, y=screenHeight*0.45)
+settingsButton.pack(side = 'top')
+#settingsButton.place(x=screenWidth*0.43, y=screenHeight*0.45)
 
 # Credits Button
 creditsButton = Button(mainWindow, text="Crédits", font=('Arial', round(screenWidth*0.016)),
                        bd=0, relief="flat", activebackground="#c59dd1", activeforeground="white", bg='#B78BC4', fg="#ffffff", width=round(screenWidth*0.006), state="disabled", command=Credits)
-creditsButton.pack(pady=20)
-creditsButton.place(x=screenWidth*0.44, y=screenHeight*0.55)
+creditsButton.pack(side = 'top')
+#creditsButton.place(x=screenWidth*0.44, y=screenHeight*0.55)
 
 # Compiling the main frame
-frame.pack(expand=YES)
 mainWindow.mainloop()
